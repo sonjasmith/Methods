f01320d450fb02a1265d340fdc2f1fbc366a0dc6
sonjasmith <sonjasmith@outlook.com>
2017-08-31 20:37:20 -0400

    First Commit

---
+++ Methods/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1 = 25;
+            int number2 = 50;
+            int total = Add(number1, number2);
+            Console.WriteLine(total);
+        }
+        public static int Add(int firstNumber, int secondNumber)
+        {
+            int sum = firstNumber + secondNumber; //place holders...don't interfere with one another because of scope
+
+            return sum; //now invoke method
+        }
+        
+        public static int Multiply(int firstNumber, int secondNumber)
+        {
+            int product = firstNumber * secondNumber;
+
+            return product;
+        }
+    }
+}
